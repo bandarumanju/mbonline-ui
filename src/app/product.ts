1ab7b4e80e9ebaaeff3fb830e380a9f0ea119d96
manjunathbandaru <bandaru.manju@gmail.com>
2024-05-23 19:52:57 -0500

    Initial setup

---
+++ src/app/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+             id: number;
+             name: string;
+             description: string;
+             category: string;
+             price: number;
+             quantity: number;
+             
+
+}
